21c044aee4240406816aacfa67676ea150578613
Yuganshu kajla <yuganshu20418@iiitd.ac.in>
2023-11-17 00:52:07 +0530

    final

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const connectToMongo = require("./db")
-const express = require('express')
+const connectToMongo = require("./db");
+const express = require('express');
 const User= require('./models/Users');
 const property= require('./models/Property');
-const cors = require('cors')
+const cors = require('cors');
 
 connectToMongo();
-const app = express()
-const port = 3000
-app.use(cors())
-app.use(express.json())
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/property',require('./routes/property'))
-app.use('/api/transaction',require('./routes/transaction'))
-app.use('/api/contract',require('./routes/contract'))
-app.use('/api/payment',require('./routes/payment'))
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/property',require('./routes/property'));
+app.use('/api/transaction',require('./routes/transaction'));
+app.use('/api/contract',require('./routes/contract'));
+app.use('/api/payment',require('./routes/payment'));
 app.get('/', (req, res) => {
   res.send('Hello, this is the root route!');
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
